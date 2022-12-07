8133506ed2c58c6d131ec3a0449b5f8af1c67b7f
HeisenbergsMeth69 <120029144+HeisenbergsMeth69@users.noreply.github.com>
2022-12-07 14:25:16 +0100

    Add files via upload

---
+++ Portfolio.php
@@ -0,0 +1,92 @@
+<?php
+  include 'headerportfolio.php';
+?>
+    <div class="row">
+      <div class="column side left"><img src="img/audiad.jpg" class="body-img"></div>
+      <div class="column side right"><img src="img/mcsad.jpg" class="body-img"></div>
+      <div class="column middle">
+        <div id="toc_container">
+          <p class="toc_title">Contents</p>
+          <ul class="toc_list">
+            <li><a href="#First_Point_Header">1 About Me</a>
+              <ul>
+                <li><a href="#main_content">1.1 Main Content</a></li>
+                <li><a href="#hobbys">1.2 Hobbys</a></li>
+                <li><a href="#my_work">1.3 Some of my work</a></li>
+              </ul>
+            </li>
+            <li><a href="#Main Content">2 Business</a></li>
+          </ul>
+        </div>
+        <h2 id="main_content">Main Content</h2>
+        <p>As said in the title, my name is Aleksandar, but people call me Aleks. I am 16 years old and I live in Germany. I love football and science. My favourite club is Manchester United and I enjoy watching Science-Fiction movies. Star Wars or The Avengers for example. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. 
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="column side left"><img src="img/mcsad.jpg" class="body-img"></div>
+  <div class="column middle">
+    <h2 id="hobbys">Hobbys</h2>
+      <p>I play football for the SV Meppen Youth. Currently I am in the U17 of the JSG Meppen and I play in the German Landesliga.
+        My other biggest hobbies are Science and Engineering. What I mean by that is that I like to build stuff, mostly different types of robots. And by Science I mean participating in olympiads and competitions in the branches of Mathematics and Physics. And other stuff I like or do are going to gym, programming, cooking and listening to music. 
+      </p>
+  </div>
+  <div class="column side right"><img src="img/mcsad.jpg" class="body-img"></div>
+    <div class="column middle">
+      <h2 id="my_work">Some of my work</h2>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+  </div>
+  <div class="main">
+    <a href="https://www.instagram.com/alex_alex8796/" target="_blank"><i class="fa fa-instagram"></i></a>
+    <a href="https://wa.me/491732460886?text=Use%20the%20chat%20only%20for%20important%20messages.%20Message%20should%20include%20your%20name%2C%20so%20I%20can%20identify%20you." target="_blank"><i class="fa fa-whatsapp"></i></a>
+    <a href="https://www.snapchat.com/add/alexander_a1907" target="_blank"><i class="fa fa-snapchat-ghost"></i></a>
+  </div>
+  <footer id="footer">
+    <div class="footer-elements">
+      <a href="login.php">Sports-Festival-Generator</a>
+    </div>
+    <div class="footer-elements">
+      <a href="signup.php" target="_blank">Sign Up</a>
+    </div>
+    <div class="footer-elements">
+      <a href="Portfolio.php" target="_blank">Home</a>
+    </div>
+    <div class="footer-elements">
+      <p>Shop</p>
+    </div>
+    <div class="footer-elements">
+      <p>Art</p>
+    </div>
+  </footer>
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
